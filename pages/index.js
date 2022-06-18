389e10e2f6d8527dd75fbfd48cd635effb9f8622
Ranjan <talk2ranjanktr@gmail.com>
2022-06-18 12:12:12 +0530

    --

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import styles from '../styles/Home.module.css'
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 
 export default function Home() {
+  //18/6/22
   return (
     <div className={styles.container}>
       <Head>
